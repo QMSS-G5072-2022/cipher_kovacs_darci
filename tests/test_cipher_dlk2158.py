8fe99579a41ce88688483fad7cc730276de66fd0
Darci Kovacs <dlk2158@columbia.edu>
2022-10-30 13:55:56 -0400

    Updating tests

--- tests/test_cipher_dlk2158.py
+++ tests/test_cipher_dlk2158.py
@@ -2,5 +2,5 @@ from cipher_dlk2158 import cipher_dlk2158
 def test_single_word():
     example = 'Hello'
     expected = 'Ifmmp'
-    actual = cipher(example,1)
+    actual = cipher_dlk2158.cipher(example,1)
     assert actual == expected
