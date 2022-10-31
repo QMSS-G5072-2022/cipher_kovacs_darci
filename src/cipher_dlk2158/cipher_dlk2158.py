8076a069d0c0290fffbd3543d21393838d2bbf39
Darci Kovacs <dlk2158@columbia.edu>
2022-10-31 11:29:52 -0400

    Updating docstring

--- src/cipher_dlk2158/cipher_dlk2158.py
+++ src/cipher_dlk2158/cipher_dlk2158.py
@@ -1,11 +1,27 @@
 
-# The Caesar cipher function can encrypt and decrypt strings of text. Each letter is replaced by a letter some fixed number of positions down the alphabet.
-
-# The input of the function has three text parameters:
-# "text" is the string to be encrypted or decrypted
-# "shift" is the fixed number of positions down the alphabet to use for encryption
-# "encrypt" is binary and tells the function to encrypt or decrypt the string
 def cipher(text, shift, encrypt=True):
+    """
+       The Caesar cipher function can encrypt and decrypt strings of text. Each letter is replaced by a letter some fixed number of positions down the alphabet.
+
+
+       Parameters
+       ----------
+       text : string: the string to be encrypted or decrypted
+       shift : integer: the fixed number of positions down the alphabet to use for encryption
+       encrypt : binary: tells the function to encrypt or decrypt the string
+
+       Returns
+       -------
+       The output of the function is the encrypted or decrypted text.
+
+       Examples
+       --------
+       from cipher_dlk2158 import cipher_dlk2158
+       cipher_dlk2158.cipher("Hello!",-1)
+       "Gdkkn!"
+       cipher_dlk2158.cipher("Gdkkn!",-1, encrypt=False)
+       "Hello!"
+       """
     alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     new_text = ''
     for c in text:
@@ -18,11 +34,6 @@ def cipher(text, shift, encrypt=True):
             new_text += alphabet[new_index:new_index+1]
     return new_text
 
-# the output of the function is the encrypted or decrypted text
 
-# For example: cipher("Hello!",-1)
-# returns 'Gdkkn!'
 
-# cipher("Gdkkn!",-1, encrypt=False)
-# returns 'Hello!'
 
